9d7c5568d989d1420a8778b8bfa2659d35d3ae3d
mathfuru <mathfuru@gmail.com>
2013-07-06 10:36:39 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Install
+-------
+You need cabal-dev.
+ 1. 
+ 2.
+ 3. 
+
+Usage
+-----
+ 1. 
+ 2.
+ 3. 
---
+++ src/Main.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE NoImplicitPrelude #-} 
+{-# LANGUAGE OverloadedStrings #-} 
+{-# OPTIONS_GHC -Wall #-}
+ 
+main :: IO ()
+main = putStrLn("hello")
