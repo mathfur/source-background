81426b59bf43b329fab3de1c1d43889d31b97732
mathfuru <mathfuru@gmail.com>
2013-07-11 23:41:11 +0900

    Add edge mark.(#15)

--- client.js
+++ client.js
@@ -33,13 +33,9 @@ ws.onmessage = function(event){
      .data(nodes)
      .enter()
      .append("rect")
-     .filter(function(d){ return top_line <= d.line && d.line <= bottom_line })
      .attr("class", "label")
-     .attr("x", function(d){ return x(d.col) })
-     .attr("y", function(d){ return y(d.line) })
-     .attr("title", function(d){ return d.word + " (" + d.col + ", " + d.line + ")" })
-     .attr("width", function(d){ return x(d.col + d.length) - x(d.col) })
-     .attr("height", function(d){ return y.rangeBand() })
+     .filter(function(d){ return top_line <= d.line && d.line <= bottom_line })
+     .attr("title", function(d){ return (d.word || '(none)') + " (" + d.col + ", " + d.line + ")" })
      .style("fill", function(d){
        if(d.count == 1){
          return "#F00";
@@ -51,5 +47,28 @@ ws.onmessage = function(event){
          return "#aaa";
        }
      })
-     .attr("stroke-width", 0);
+     .attr("stroke-width", 0)
+     .call(set_location);
+
+  svg.selectAll(".edge_mark")
+     .data([
+         {col: 0,             line: top_line,        length: 1},
+         {col: 0,             line: bottom_line - 1, length: 1},
+         {col: win_width - 2, line: top_line,        length: 1},
+         {col: win_width - 2, line: bottom_line - 1, length: 1}
+     ])
+     .enter()
+     .append("rect")
+     .attr("class", "edge_mark")
+     .attr("title", "edge")
+     .call(set_location)
+     .attr("fill", "#000");
+
 };
+
+function set_location(e){
+  return e.attr("x", function(d){ return x(d.col) })
+          .attr("y", function(d){ return y(d.line) })
+          .attr("width", function(d){ return x(d.col + d.length) - x(d.col) })
+          .attr("height", function(d){ return y.rangeBand() });
+}
